65a36779acd869c71598b09f17e388a6d5b1b48d
01pooja10 <kuchan2001@gmail.com>
2020-09-18 12:55:48 +0530

    Add files via upload

--- Streamlit_app/ASL_app.py
+++ Streamlit_app/ASL_app.py
@@ -1,4 +1,4 @@
-#import the required modules
+#import necessary modules
 import streamlit as st
 import numpy as np
 import tensorflow as tf
@@ -7,6 +7,8 @@ from keras.preprocessing.image import img_to_array,array_to_img
 from PIL import Image,ImageOps
 import matplotlib.pyplot as plt
 import string
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 #main page details
 st.title('Sign Language Gesture Recognition Model')
@@ -17,20 +19,22 @@ menu=['Welcome','Description','App']
 c=st.sidebar.selectbox('Menu',menu)
 if c=='Welcome':
     st.subheader('Welcome to our project')
+    st.write('The main motivation behind this project is the benefit it provides by bridging the gaps in communication for people with hearing or speech related disabilities.')
+    st.write('The project aims to successfully translate input images of sign language gestures to the respective alphabets or numbers.')
     st.write('The model has been trained on alphabets and digits using a dataset from Kaggle and will hence make predictions accordingly.')
-    st.write('The convolutional neural network used, consists of 3 convolutional layers and a fully connected dense layer.')
+    st.write("Read more about the model in the 'Description' section.")
 elif c=='Description':
     st.title('Model description')
     st.write("A 3 layer convolutional neural network was used to build the model. This architecture along with a fully connected dense layer was fed to the output layer which used softmax activation function consisting of 37 units.")
     st.write("The model was then compiled using the 'adam' optimizer. The model, with a batch size of 128, was trained on 44,000 images for 10 epochs. It resulted in a training accuracy of 0.9148 and validation accuracy of 0.9995.")
     st.write("The model was also evaluated on the testing data. It churned out an accuracy of 0.9996.")
     st.write("Since the model was trained on augmented images of few people performing all the gestures in similar backgorunds, the predictions on real world data may not be entirely accurate.")
-    st.write("The model can be tested by uploading an image corresponding to any gesture. The prediction will be displayed instantly.")
+    st.write("The model can be tested by uploading an image corresponding to any gesture in the 'App' section. The prediction will be displayed instantly.")
 elif c=='App':
     st.title('Welcome to the working model.')
     up_file=st.file_uploader("Please upload your input image",type=['jpg','png'])
 
-    #define a function for taking inputs and printing the outputs
+    #define a function to read input images and display predictions
     def image_stuff(img1):
         img=np.asarray(img1)
         img= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -51,12 +55,11 @@ elif c=='App':
         model=tf.keras.models.load_model('/content/MIC_project.h5')
         model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
         model.load_weights('/content/MIC_project.h5')
-
         new_pred=model.predict(eg[:1])
         st.write('Running predictions...')
         ynew=np.argmax(new_pred, axis=-1)
 
-        #assign the classes for alphabets and numbers
+        #assign the classes
         if(ynew>=10):
             if (ynew==10):
                 ynew='A'
@@ -138,7 +141,7 @@ elif c=='App':
         else:
             st.write("Unknown gesture. Try again.")
 
-    #check the file uploaded
+    #check the file uploaded by the user
     if up_file is not None:
         img1=Image.open(up_file)
         st.image(img1,caption='Image uploaded by you')
